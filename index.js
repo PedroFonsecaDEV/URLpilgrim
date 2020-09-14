e0c4a4481f5395cc5187661991ddc4567071c64d
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-09-14 18:24:08 -0400

    License

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const minimist = require("minimist");
+
 module.exports= () => {
-    console.log("welcome to URLpilgrim");
+    const args = minimist(process.argv.slice(2));
+    const cmd = args._[0];
+    console.log("cm:", cmd);
+    console.log("ar:", args);
+
+   
 }
