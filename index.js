ed90d491be78a84f2334f0bba1b33b81f0d2f5f7
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-09-15 04:06:36 -0400

    Regex Upgrade

--- index.js
+++ index.js
@@ -12,30 +12,37 @@ module.exports= () => {
     const urlRegex = /(http|https)(:\/\/)([\w+\-&@`~#$%^*.=/?:]*)/gi;
     
     const fileStream = readLine.createInterface({ 
-        input: fs.createReadStream("url_test2.txt"), 
+        input: fs.createReadStream("1.html"), 
         output: null,
         terminal: false
     }); 
     
     let urlList = [];
     let iterator = [];
+    let counter = 0;
 
     // urlList = urlRegex.exec("http://s9y.org");
     // console.log("r", urlList);
 
+    
     fileStream.on('line', (input) => {
         
         while ((iterator = urlRegex.exec(input)) !== null) {
             let msg = 'Found ' + iterator[0] + '. ';
             msg += 'Next match starts at ' + urlRegex.lastIndex;
             console.log(msg);
-          
+            counter++;
             urlList.push(iterator);
         }
         
         
       });
-      
+  
+    
+
+    
+
+    setTimeout(() => console.log(counter), 10000);
 
     
 
