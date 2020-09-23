99e3b102ff88bfb0a7e6a5aa8ec657225cac8243
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-09-23 14:59:22 -0400

    b

--- urlFunctions/urlFuncs.js
+++ urlFunctions/urlFuncs.js
@@ -44,7 +44,30 @@ const testUrl = async (urlArray) => {
    };
     
   }
+  // const testUrl = async (urlArray) => {
+  //   const protocolRegex = /^www(.+)/gi;
+  //   const spinner = ora("URL PILGRIM - Checking URLs Status").start();
+  //   for (let url of urlArray) {
+  //     if (protocolRegex.test(url)) {
+  //       url = "https://" + url;
+  //     }
   
+  //     fetch(url, { method: "head" })
+  //       .then((urlTest) => {
+  //         if (urlTest.status == 200) {
+  //           printLog(`URL: ${url} Status: ${urlTest.status}`, "good");
+  //         } else if (urlTest.status == 400 || urlTest.status == 404) {
+  //           printLog(`URL: ${url} Status: ${urlTest.status}`, "bad");
+  //         } else {
+  //           printLog(`URL: ${url} Status: Unknown`, "unknown");
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         printLog(`URL: ${url} Status: Unknown`, "unknown");
+  //       });
+  //   }
+  //   spinner.stop();
+  // };
 
 module.exports = {
     checkDuplicateUrl,
