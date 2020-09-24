4f3663d199a738b2b64fef5895b33de6d5614484
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-09-24 04:15:52 -0400

    Update 0.0.10

--- urlFunctions/urlFuncs.js
+++ urlFunctions/urlFuncs.js
@@ -28,17 +28,18 @@ const testUrl = async (urlArray) => {
     const urlTest = await fetch(url,{method: "head", timeout: 1500});
     spinner.stop();   
      if(urlTest.status == 200){
-      printLog(`URL: ${url} Status: ${urlTest.status}`, "good");
+      printLog(`URL: ${url} Status: GOOD`, "good");
       }
       else if(urlTest.status == 400 || urlTest.status == 404 ){
-      printLog(`URL: ${url} Status: ${urlTest.status}`, "bad");
+      printLog(`URL: ${url} Status: BAD`, "bad");
       }
       else{
       printLog(`URL: ${url} Status: Unknown`, "unknown");
       } 
     }catch (error) {
       spinner.stop(); 
-      printLog(`URL: ${url} Status: ${urlTest.status}`, "bad");
+      printLog(`URL: ${url} Status: BAD`, "bad");
+      
     } 
     
    };
