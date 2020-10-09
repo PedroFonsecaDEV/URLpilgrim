e339eaf161878e8a007d97b36eaddbfec60da319
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-10-09 00:07:52 -0400

    update

--- src/url_functions/url-funcs.js
+++ src/url_functions/url-funcs.js
@@ -3,36 +3,33 @@ const ora = require("ora");
 const { printLog } = require("../console_messages/console-msg");
 
 const testUrl = async (urlArray) => {
-  const protocolRegex = /^www(.+)/gi
+  const protocolRegex = /^www(.+)/gi;
 
-  for(let url of urlArray){
-    
-    if(protocolRegex.test(url)){
-      url = "https://" + url; 
+  for (let url of urlArray) {
+    if (protocolRegex.test(url)) {
+      url = "https://" + url;
     }
 
     const spinner = ora("URL PILGRIM - Checking URLs Status").start();
-    try{ 
-    const urlTest = await fetch(url,{method: "head", timeout: 1500});
-    spinner.stop();   
-     if(urlTest.status == 200){
-      printLog(`URL: ${url} Status: GOOD`, "good");
-      }
-      else if(urlTest.status == 400 || urlTest.status == 404 ){
-      printLog(`URL: ${url} Status: BAD`, "bad");
-      }
-      else{
-      printLog(`URL: ${url} Status: Unknown`, "unknown");
-      } 
-    }catch (error) {
-      spinner.stop();
-      printLog(`URL: ${url} Status: BAD`, "bad");
-    } 
-    
-   };
-    
+
+    fetch(url, { method: "head", timeout: 1500 })
+      .then((urlTest) => {
+        spinner.stop();
+        if (urlTest.status == 200) {
+          printLog(`URL: ${url} Status: GOOD`, "good");
+        } else if (urlTest.status == 400 || urlTest.status == 404) {
+          printLog(`URL: ${url} Status: BAD`, "bad");
+        } else {
+          printLog(`URL: ${url} Status: Unknown`, "unknown");
+        }
+      })
+      .catch((error) => {
+        spinner.stop();
+        printLog(`URL: ${url} Status: BAD`, "bad");
+      });
   }
+};
 
 module.exports = {
-    testUrl
+  testUrl,
 };
