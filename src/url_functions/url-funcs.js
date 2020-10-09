97b24ccf253276a0166782a3af644a64dfaf2131
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-10-09 00:34:03 -0400

    update

--- src/url_functions/url-funcs.js
+++ src/url_functions/url-funcs.js
@@ -1,6 +1,4 @@
 const fetch = require("node-fetch");
-const { promise } = require("ora");
-const ora = require("ora");
 const { printLog } = require("../console_messages/console-msg");
 
 const testUrl = async (urlArray) => {
@@ -12,18 +10,18 @@ const testUrl = async (urlArray) => {
     }
 
     fetch(url, { method: "head", timeout: 1500 })
-      .then((urlTest) => {
-        if (urlTest.status == 200) {
-          printLog(`URL: ${url} Status: GOOD`, "good");
-        } else if (urlTest.status == 400 || urlTest.status == 404) {
-          printLog(`URL: ${url} Status: BAD`, "bad");
-        } else {
-          printLog(`URL: ${url} Status: Unknown`, "unknown");
-        }
-      })
-      .catch((error) => {
+    .then((urlTest) => {
+      if (urlTest.status == 200) {
+        printLog(`URL: ${url} Status: GOOD`, "good");
+      } else if (urlTest.status == 400 || urlTest.status == 404) {
         printLog(`URL: ${url} Status: BAD`, "bad");
-      });
+      } else {
+        printLog(`URL: ${url} Status: Unknown`, "unknown");
+      }
+    })
+    .catch((error) => {
+      printLog(`URL: ${url} Status: BAD`, "bad");
+    });
   }
 };
 
