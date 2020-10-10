dbb46ba378ba1fa27422ebab965acbd7e53ef37c
Pedro Fonseca <60857409+PedroFonsecaDEV@users.noreply.github.com>
2020-10-10 02:28:54 -0400

    json output done

--- src/url_functions/url-funcs.js
+++ src/url_functions/url-funcs.js
@@ -1,37 +1,48 @@
 const fetch = require("node-fetch");
 const { printLog } = require("../console_messages/console-msg");
 
-const testUrl = async (urlArray, filterResult = null) => {
-  const protocolRegex = /^www(.+)/gi;
-  
-  const urlsPromises = urlArray.map((url) =>{
+const testUrl = (urlArray, filterResult = null, output = false) => {
+  const fetchUrl = async(url) => {
+    const protocolRegex = /^www(.+)/gi;
+    
     if (protocolRegex.test(url)) {
       url = "https://" + url;
     }
-  
-    const urlRes = await fetch(url, { method: "head", timeout: 1500 });
 
-  })
-    
+    try {
+    const urlResult = await fetch(url, { method: "head", timeout: 1500 });
+    return {url: url, status: urlResult.status};
+    } 
+    catch (error) {
+      return {url: url, status: 400};
+    }
+  };
 
-    
-    .then((urlTest) => {
-      if(filterResult === null){
-        if (urlTest.status == 200) printLog(`URL: ${url} Status: 200`, 200);
-        else if (urlTest.status == 400 || urlTest.status == 404) printLog(`URL: ${url} Status: 400`, 400);
-        else printLog(`URL: ${url} Status: Unknown`, 9999); 
+  const urlPromises = urlArray.map(fetchUrl);
+  Promise.all(urlPromises)
+  .then(finalResults => finalResults.map(urlTest => {
+    if(filterResult === null){
+      if (urlTest.status == 200) printLog(`URL: ${urlTest.url} Status: 200`, 200);
+      else if (urlTest.status == 400 || urlTest.status == 404) printLog(`URL: ${urlTest.url} Status: 400`, 400);
+      else printLog(`URL: ${urlTest.url} Status: Unknown`, 9999);
       }
       else {
-        if((urlTest.status == filterResult) || (filterResult == 400 && urlTest.status == 404)) printLog(`URL: ${url} Status: ${filterResult}`, filterResult);
-        else if((filterResult == 9999) && urlTest.status != 200 && urlTest.status && 400 && urlTest.status != 404 ) printLog(`URL: ${url} Status: Unknown`, 9999);
+        if((urlTest.status == filterResult) || (filterResult == 400 && urlTest.status == 404)) printLog(`URL: ${urlTest.url} Status: ${filterResult}`, filterResult);
+        else if((filterResult == 9999) && urlTest.status != 200 && urlTest.status && 400 && urlTest.status != 404 ) printLog(`URL: ${urlTest.url} Status: Unknown`, 9999);
       }
-    })
-    .catch((error) => {
-      if(!filterResult)printLog(`URL: ${url} Status: 400`, 400);
-    });
-  }
+      return urlTest
+  }))
+  .then((results) => {
+    console.log("FINAL RESULT JSON:");
+    if(output) console.log(JSON.stringify(results));
+  })
+  .catch((urlObj) => {
+    console.log(urlObj);
+    if(!filterResult)printLog(`URL: ${urlObj.url} Status: 400`, 400);
+  });
 };
+    
 
 module.exports = {
-  testUrl,
+  testUrl
 };
