addbe6ad17a03e36c7adba556a1de3c58a309970
PedroFonsecaDEV <>
2020-11-23 02:36:52 -0500

    Eslint: jest/global

---
+++ src/file_reader/index.test.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-useless-path-segments
+
+const { readFiles } = require('../file_reader');
+
+function sum(a, b) {
+  return a + b;
+}
+
+describe('test readFiles function', () => {
+  function checkOutput(output, optionSent) {
+    expect(Array.isArray(output)).toBe('true');
+  }
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
