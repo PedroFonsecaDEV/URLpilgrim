635610ce8251165febc455f9b6165b6dfc5b9913
PedroFonsecaDEV <pedroporventura@gmail.com>
2020-11-26 14:16:25 -0500

    thursday start

--- src/tests/urlFunctions.test.js
+++ src/tests/urlFunctions.test.js
@@ -2,15 +2,42 @@ const { testUrl } = require('../url_functions');
 const { printLog } = require('../console_messages');
 
 const nock = require('nock');
-const console_messages = require('../console_messages');
+const originalConsolelog = global.console.log;
 
-test('testing nock with node-fetch', async () => {
-  const host = 'https://www.youtube.com';
-  const path = '/';
-  nock(host).head(path).reply(200);
+describe('url module -> testUrl', () => {
+  let logOutput = null;
 
-  const url = `${host}${path}`;
-  const result = await testUrl([url]);
-  expect(printLog('URL: https://www.youtube.com/ Status: 200', 200)).toEqual(printLog(expected));
-  expect(typeof result).toBe(true);
+  function testLogFn(...args) {
+    logOutput = logOutput || [];
+    args.forEach((arg) => logOutput.push(arg));
+  }
+
+  function finalize(output) {
+    if (output && Array.isArray(output)) {
+      return output.join('');
+    }
+    return output;
+  }
+
+  beforeEach(() => {
+    global.console.log = testLogFn;
+    logOutput = null;
+  });
+
+  afterEach(() => {
+    global.console.log = originalConsolelog;
+    logOutput = null;
+  });
+
+  test('testing nock with node-fetch', async () => {
+    const host = 'https://www.youtube.com';
+    const path = '/';
+    nock(host).head(path).reply(200);
+
+    const url = `${host}${path}`;
+    const result = await testUrl([url]);
+    const expected = 'URL: https://www.youtube.com/ Status: 200';
+
+    expect(finalize(logOutput)).toEqual(expected);
+  });
 });
